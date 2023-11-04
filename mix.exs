e9490733ffb0f7eac9f19e7e48af1023f140c135
Matthew Whitworth <matt@tallarium.com>
2023-11-04 12:19:25 +0000

    0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MoxInject.MixProject do
   def project do
     [
       app: :mox_inject,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
